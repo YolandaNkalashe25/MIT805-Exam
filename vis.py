57269dd517d6b843087c318e09090bd081e8ea9c
YolandaNkalashe25 <70112102+YolandaNkalashe25@users.noreply.github.com>
2021-06-16 14:58:48 +0200

    Add files via upload

--- vis.py
+++ vis.py
@@ -308,22 +308,22 @@ def main():
 # =============================================================================
                    st.pyplot(fig)   
                    
-                   import pickle
+                   #import pickle
 # =============================================================================
 #                    def Cat_Model():
 #                             import joblib
 #                             pred_model = joblib.load('classifier_SACat.pkl.pkl')
 #                             return pred_model
 # =============================================================================
-                   pickle_in = open("classifier_SACat.pkl.pkl", "rb") 
-                   Cat_Model=pickle.load(pickle_in)   
-                   clean_cat=CategoriseSA(data_processed)
+                   #pickle_in = open("classifier_SACat.pkl.pkl", "rb") 
+                   #Cat_Model=pickle.load(pickle_in)   
+                   #clean_cat=CategoriseSA(data_processed)
       
                    #pred_model=Cat_Model() 
                    
-                   categorise=Cat_Model.predict(clean_cat.statuses_without_stopwords)
+                   #categorise=Cat_Model.predict(clean_cat.statuses_without_stopwords)
                    
-                   st.write(categorise)  
+                   #st.write(categorise)  
 #                    categorise=categorise.tolist()
 #                    
 #                    df_class=pd.DataFrame(categorise,columns=["Class_Label"])
